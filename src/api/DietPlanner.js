6205485aeb0a6f6686039aba4283a3a4af7e16a3
godivyang <godivyang@users.noreply.github.com>
2025-09-01 22:26:05 +0530

    Bug fix

--- src/api/DietPlanner.js
+++ src/api/DietPlanner.js
@@ -12,7 +12,7 @@ const checkIfLogin = async (code) => {
         const response = await axiosInstance.post("/user/me", { code });
         return response.data.data;
     } catch (e) {
-        alert(e.details.message)
+        alert(e.toString())
         throw new Error({"message": e.details.message});
     }
 };
